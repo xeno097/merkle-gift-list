cbb30e29d13f6e6450596eeddc372d3c2d79400f
Sebastiano Faiella <xeno097.cp@gmail.com>
2022-12-11 13:58:49 -0400

    feat(client): started working on the get-root subcommand

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,23 +1,58 @@
-use clap::Parser;
+use std::{error::Error, fs};
 
-/// Client to validate if a person is in the Gift List
+use clap::{Command, FromArgMatches, Parser, Subcommand};
+use merkle_tree::{self, MerkleeTree};
+
 #[derive(Parser, Debug)]
-struct Args {
+struct GetRootArgs {
+    /// File that stores the list of allowed names to receive a gift
+    #[arg(long, default_value = "nice-list.json")]
+    file: String,
+}
+
+#[derive(Parser, Debug)]
+struct ValidateArgs {
+    /// File that stores the list of allowed names to receive a gift
+    #[arg(long, default_value = "nice-list.json")]
+    file: String,
+
     /// Host of the server that will validate the data
     #[arg(long, default_value = "127.0.0.1:3000")]
     host: String,
-
-    /// File that stores the allowed names to receive a gift
-    #[arg(long, default_value = "nice-list.json")]
-    file: String,
 
     /// Name of the person to validate if it is in the list
     #[arg(long)]
     name: String,
 }
 
-fn main() {
-    let args = Args::parse();
+#[derive(Parser, Debug)]
+enum Subcommands {
+    GetRoot(GetRootArgs),
+    Validate(ValidateArgs),
+}
 
-    println!("{:#?}", args)
+fn main() -> Result<(), Box<dyn Error>> {
+    let cli = Command::new("Built CLI");
+
+    let cli = Subcommands::augment_subcommands(cli);
+
+    let matches = cli.get_matches();
+    let derived_subcommands = Subcommands::from_arg_matches(&matches)?;
+
+    match derived_subcommands {
+        Subcommands::GetRoot(args) => {
+            let raw_list = fs::read_to_string(args.file)?;
+
+            let gift_list: Vec<String> = serde_json::from_str(&raw_list)?;
+
+            let merkle_tree_gift_list = MerkleeTree::new(gift_list);
+
+            let root = merkle_tree_gift_list.get_root();
+
+            println!("{:#?}", root);
+        }
+        Subcommands::Validate(_) => todo!(),
+    }
+
+    Ok(())
 }
