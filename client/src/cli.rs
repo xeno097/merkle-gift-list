add790eb2003c3c6693e4fbf855b05e4dbbb6079
Sebastiano Faiella <xeno097.cp@gmail.com>
2022-12-11 15:29:00 -0400

    feat(client): completed implementation of the get-root command

--- client/src/cli.rs
+++ client/src/cli.rs
@@ -1,28 +1,36 @@
 use std::error::Error;
 
-use clap::{Command, FromArgMatches, Parser, Subcommand};
+use clap::Parser;
 
 use crate::{
     get_root::{get_root, GetRootArgs},
     validate::ValidateArgs,
 };
 
-#[derive(Parser, Debug)]
-enum Subcommands {
+#[derive(Parser)]
+enum Commands {
+    /// Builds a merkle root from <FILE>
     GetRoot(GetRootArgs),
+
+    /// Validates that <NAME> belongs to the merkle tree by querying <HOST>
     Validate(ValidateArgs),
 }
 
+#[derive(Parser)]
+struct Cli {
+    /// File that stores the list of allowed names to receive a gift
+    #[arg(short, long, default_value = "nice-list.json")]
+    file: String,
+
+    #[clap(subcommand)]
+    subcommand: Commands,
+}
+
 pub fn run_cli() -> Result<(), Box<dyn Error>> {
-    let cli = Command::new("Built CLI");
+    let args = Cli::parse();
 
-    let cli = Subcommands::augment_subcommands(cli);
-
-    let matches = cli.get_matches();
-    let derived_subcommands = Subcommands::from_arg_matches(&matches)?;
-
-    match derived_subcommands {
-        Subcommands::GetRoot(args) => get_root(args),
-        Subcommands::Validate(_) => todo!(),
+    match args.subcommand {
+        Commands::GetRoot(_) => get_root(&args.file),
+        Commands::Validate(_) => todo!(),
     }
 }
--- client/src/get_root.rs
+++ client/src/get_root.rs
@@ -3,23 +3,17 @@ use std::{error::Error, fs};
 use clap::Parser;
 use merkle_tree::{self, MerkleeTree};
 
-#[derive(Parser, Debug)]
-pub struct GetRootArgs {
-    /// File that stores the list of allowed names to receive a gift
-    #[arg(long, default_value = "nice-list.json")]
-    file: String,
-}
+#[derive(Parser)]
+pub struct GetRootArgs {}
 
-pub fn get_root(args: GetRootArgs) -> Result<(), Box<dyn Error>> {
-    let raw_list = fs::read_to_string(args.file)?;
+pub fn get_root(file_path: &str) -> Result<(), Box<dyn Error>> {
+    let raw_list = fs::read_to_string(file_path)?;
 
     let gift_list: Vec<String> = serde_json::from_str(&raw_list)?;
 
-    let merkle_tree_gift_list = MerkleeTree::new(gift_list);
+    let root = MerkleeTree::new(gift_list).get_root();
 
-    let root = merkle_tree_gift_list.get_root();
-
-    println!("{:#?}", root);
+    println!("Merkle root: {root}");
 
     Ok(())
 }
